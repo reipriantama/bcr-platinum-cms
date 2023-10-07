c173e0bcc99281f5844f7b60278296f147864912
FaishalD <88101023+FaishalD@users.noreply.github.com>
2023-10-07 10:46:22 +0700

    Feature/dashboard header (#5)
    * feature: menambahkan header
    
    * feature:menambahkan logo, icon menu dan memperbaiki kolom search

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../../components/Dashboard/Header/Header'
 
 const DashboardPage = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
