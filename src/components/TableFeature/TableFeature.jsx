267b4a0cf75bc775574617b1089c0cf710364844
reipriantama <141336153+reipriantama@users.noreply.github.com>
2023-10-17 20:30:26 +0700

    Feature/carspage carlist (#14)
    * feature: menambahkan car list pada halaman cars
    
    * feature: memperbaiki feature search dan carlist pada halaman car

--- src/components/TableFeature/TableFeature.jsx
+++ src/components/TableFeature/TableFeature.jsx
@@ -4,12 +4,33 @@ import BootstrapTable from "react-bootstrap-table-next";
 import moment from "moment";
 import styles from "./TableFeature.module.css";
 import paginationFactory from "react-bootstrap-table2-paginator";
-
+import { FaSort } from "react-icons/fa";
+// import { Dropdown } from "react-bootstrap";
 
 const TableFeature = () => {
   const [tableData, setTableData] = useState([]);
   const [page, setPage] = useState(1);
   const [sizePerPage, setSizePerPage] = useState(5); // Misalnya, ukuran halaman 10
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.getOrder({ pageSize: 50 });
+        console.log(response);
+        if (response.status === 200) {
+          // Jika respons sukses
+          const orderData = response.data.orders;
+          setTableData(orderData); // Mengatur data tabel dengan hasil respons
+          console.log(orderData);
+        } else {
+          console.error("Gagal mengambil data getOrder");
+        }
+      } catch (error) {
+        console.error("Terjadi kesalahan:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handlePageChange = (newPage) => {
     setPage(newPage);
@@ -33,25 +54,6 @@ const TableFeature = () => {
     return moment(date).format("DD MMMM YYYY");
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await api.getOrder({});
-        if (response.status === 200) {
-          // Jika respons sukses
-          const orderData = response.data.orders;
-          setTableData(orderData); // Mengatur data tabel dengan hasil respons
-          console.log(orderData);
-        } else {
-          console.error("Gagal mengambil data getOrder");
-        }
-      } catch (error) {
-        console.error("Terjadi kesalahan:", error);
-      }
-    };
-    fetchData();
-  }, []);
-
   const columns = [
     {
       dataField: "index",
@@ -61,42 +63,72 @@ const TableFeature = () => {
     },
     {
       dataField: "User.email",
-      text: "User Email",
+      text: (
+        <span className="d-flex justify-content-between align-items-center">
+          User Email
+          <FaSort />
+        </span>
+      ),
       sort: true,
     },
     {
       dataField: "Car",
-      text: "Car",
+      text: (
+        <span className="d-flex justify-content-between align-items-center">
+          Car
+          <FaSort />
+        </span>
+      ),
       sort: true,
       formatter: (cell) => {
-        return cell ? cell: "car" 
-      }
+        return cell ? cell : "car";
+      },
     },
     {
       dataField: "start_rent_at",
-      text: "Start Rent",
+      text: (
+        <span className="d-flex justify-content-between align-items-center">
+          Start Rent
+          <FaSort />
+        </span>
+      ),
       sort: true,
       formatter: (cell) => formatDate(cell),
     },
     {
       dataField: "finish_rent_at",
-      text: "Finish Rent",
+      text: (
+        <span className="d-flex justify-content-between align-items-center">
+          Finish Rent
+          <FaSort />
+        </span>
+      ),
       sort: true,
       formatter: (cell) => formatDate(cell),
     },
     {
       dataField: "total_price",
-      text: "Price",
+      text: (
+        <span className="d-flex justify-content-between align-items-center">
+          Price
+          <FaSort />
+        </span>
+      ),
       sort: true,
       formatter: (cell, row) => formatRupiah(cell),
     },
     {
       dataField: "category",
-      text: "Category",
+      text: (
+        <span className="d-flex justify-content-between align-items-center">
+          Category
+          <FaSort />
+        </span>
+      ),
       sort: true,
       formatter: (cell) => {
-        return cell ? cell: "Category" 
-      }
+        return cell ? cell : "Category";
+      },
     },
   ];
 
@@ -120,10 +152,35 @@ const TableFeature = () => {
           onPageChange: handlePageChange,
           onSizePerPageChange: handleSizePerPageChange,
           sizePerPageList: [
-            { text: "3", value: 3 },
             { text: "5", value: 5 },
             { text: "10", value: 10 },
+            { text: "15", value: 15 },
           ],
+          alwaysShowAllBtns: true,
+          // bootstrap4: true,
+          // // sizePerPageRenderer: ({
+          // //   options,
+          // //   currSizePerPage,
+          // //   onSizePerPageChange,
+          // // }) => (
+          // //   <div className="custom-size-per-page">
+          // //     <Dropdown>
+          // //       <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          // //         {currSizePerPage}
+          // //       </Dropdown.Toggle>
+          // //       <Dropdown.Menu className={styles.dropDownMenu}>
+          // //         {options.map((option) => (
+          // //           <Dropdown.Item
+          // //             key={option.text}
+          // //             onClick={() => onSizePerPageChange(option.value)}
+          // //           >
+          // //             {option.text}
+          // //           </Dropdown.Item>
+          // //         ))}
+          // //       </Dropdown.Menu>
+          // //     </Dropdown>
+          // //   </div>
+          // // ),
         })}
       />
     </div>
