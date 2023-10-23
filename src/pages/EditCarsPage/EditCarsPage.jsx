fc64ca535bbe5f1767015ac1548606a5c7efd2ba
reipriantama <141336153+reipriantama@users.noreply.github.com>
2023-10-23 14:43:48 +0700

    feature: membuat logic untuk halaman edit (#17)

--- src/pages/EditCarsPage/EditCarsPage.jsx
+++ src/pages/EditCarsPage/EditCarsPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import EditCars from "../../components/EditCars/EditCars";
+import { useParams } from "react-router";
 
 const EditCarsPage = () => {
+  const { carId } = useParams();
+  console.log(carId);
   return (
     <div>
-      <EditCars />
+      <EditCars id={carId} />
     </div>
   );
 };
